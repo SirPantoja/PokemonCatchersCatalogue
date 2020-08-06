5ef018a1ef879a3a1ce84355fcb14bd086c96b89
Andy Pantoja <sirpantoja@fb.com>
2020-08-06 14:04:41 -0700

    Added login animation

--- app/src/main/java/com/example/pokemoncatcherscatalogue/MainActivity.java
+++ app/src/main/java/com/example/pokemoncatcherscatalogue/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.pokemoncatcherscatalogue;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
